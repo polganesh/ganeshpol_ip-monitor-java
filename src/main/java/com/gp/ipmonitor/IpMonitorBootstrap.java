3d6c1bfe5bb219501dd88e29b75c141f5027a622
Ganesh Pol <polganesh@gmail.com>
2018-09-15 00:18:11 +0530

    initial version with actuator

---
+++ src/main/java/com/gp/ipmonitor/IpMonitorBootstrap.java
@@ -0,0 +1,12 @@
+package com.gp.ipmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpMonitorBootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpMonitorBootstrap.class, args);
+	}
+}
