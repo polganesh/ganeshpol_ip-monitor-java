5fb43d0ab20f9e4fee7dd40428c06ba09338c65b
Ganesh Pol <polganesh@gmail.com>
2018-09-17 07:13:36 +0530

    impl of last remaining API which missed earlier

--- src/main/java/com/gp/ipmonitor/query/rest/CheckIpIsBlacklistedForSystemController.java
+++ src/main/java/com/gp/ipmonitor/query/rest/CheckIpIsBlacklistedForSystemController.java
@@ -3,17 +3,40 @@
  */
 package com.gp.ipmonitor.query.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gp.ipmonitor.query.rest.service.IGetIpConfigDetailsForSystemService;
+
 /**
  * @author GANESH
  *
  */
 public class CheckIpIsBlacklistedForSystemController {
 
+	@Autowired
+	IGetIpConfigDetailsForSystemService service;
+
+	
 	/**
 	 * 
 	 */
 	public CheckIpIsBlacklistedForSystemController() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	@RequestMapping(
+			value = "/is-black-listed", 
+			method = RequestMethod.GET, 
+			headers = "Accept=application/json")
+	@ResponseBody
+	public ResponseEntity<Boolean>  isBlackListedForSystem(@RequestParam("systemId") String systemId,@RequestParam("ip") String ip){
+		return new  ResponseEntity<Boolean>(service.isIpBlacklistedForSystem(systemId,ip), HttpStatus.OK);
 	}
 
 }
